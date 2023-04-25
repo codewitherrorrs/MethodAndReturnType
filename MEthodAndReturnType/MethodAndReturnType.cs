e70ba14e6aacab0fd40e7060a2e7f5928a7b52dc
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-25 19:07:19 +0530

    Add : Method And Return Type

---
+++ MEthodAndReturnType/MethodAndReturnType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEthodAndReturnType
+{
+    public class MethodAndReturnType
+    {
+        //Method without parameter.
+        public void Method()
+        {
+            Console.WriteLine("Method Without Parameter");
+        }
+        //Method With Parameter
+        public void MethodOne(int x)
+        {
+            Console.WriteLine($"Method Without Parameter : {x}");
+        }
+        //Method With Return Type.
+        public int MethodTwo()
+        {
+            return 10;
+        }
+        //Method With Return type String.
+        public string MethodThree()
+        {
+            return "Shreya ma'am";
+        }
+        //Method With Bool Return Type
+        public bool MethodFour()
+        {
+            return true;
+        }
+    }
+}
---
+++ MEthodAndReturnType/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodAndReturnType
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            MethodAndReturnType obj = new MethodAndReturnType();
+            obj.Method();
+            obj.MethodOne(10);
+            Console.WriteLine(obj.MethodTwo());
+            Console.WriteLine(obj.MethodThree());
+            Console.WriteLine(obj.MethodFour());
+            Console.ReadKey();
+        }
+    }
+}
